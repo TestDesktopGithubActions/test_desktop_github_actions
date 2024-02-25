fe4b950eebfa17bbc6086bda06db346474254cfe
QuantumCat957 <QuantumCat957@github.com>
2024-02-26 00:02:25 +0800

    test

---
+++ scripts/publish.mjs
@@ -0,0 +1,8 @@
+import cp from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+const APP_VERSION = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' })).version;
+const TAG = `v${APP_VERSION}`;
+
+cp.execSync(`gh release create ${TAG} -R https://github.com/TestDesktopGithubActions/desktop_release`, { stdio: 'inherit' });
