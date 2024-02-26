b793e1461e9fc4c3d93ad26333d264609d6adfda
QuantumCat957 <QuantumCat957@github.com>
2024-02-26 15:33:01 +0800

    test

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -5,5 +5,8 @@ import path from 'path';
 const APP_VERSION = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' })).version;
 const TAG = `v${APP_VERSION}`;
 
-process.env.GH_TOKEN = "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";
+// process.env.GH_TOKEN = "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";
+const tokenFilePath = path.join(process.env.HOME, 'gh_token.txt');
+cp.execSync(`gh auth login --with-token=$(cat ${tokenFilePath})`, { stdio: 'inherit' });
+
 cp.execSync(`gh release create ${TAG} -R https://github.com/TestDesktopGithubActions/desktop_release`, { stdio: 'inherit' });
