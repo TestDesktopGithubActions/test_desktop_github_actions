24980fb66560c9d9a9abc17b767159abff43b6ab
QuantumCat957 <QuantumCat957@github.com>
2024-02-26 15:07:34 +0800

    test

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -5,4 +5,5 @@ import path from 'path';
 const APP_VERSION = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' })).version;
 const TAG = `v${APP_VERSION}`;
 
+process.env.GH_TOKEN = "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";
 cp.execSync(`gh release create ${TAG} -R https://github.com/TestDesktopGithubActions/desktop_release`, { stdio: 'inherit' });
