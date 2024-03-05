a811617323a18acbec516f7c4544dd35411a07d3
QuantumCat957 <QuantumCat957@github.com>
2024-03-06 03:49:53 +0800

    test

--- scripts/prepareArtifactPaths.mjs
+++ scripts/prepareArtifactPaths.mjs
@@ -1,4 +1,5 @@
 const paths = process.env.ARTIFACT_PATHS;
 const parsedPaths = JSON.parse(paths);
 const artifactPaths = Array.isArray(parsedPaths) ? parsedPaths : [parsedPaths]; // 将单个路径转为数组
-console.log(artifactPaths.join("\n"));
+// console.log(artifactPaths.join("\n"));
+process.stdout.write(artifactPaths.join("\n"));
