e54f1a94e98274f8fc45c5acd57377f1852facf9
QuantumCat957 <QuantumCat957@github.com>
2024-03-21 17:22:35 +0800

    test

--- scripts/prepareArtifactPaths.mjs
+++ scripts/prepareArtifactPaths.mjs
@@ -1,11 +1,16 @@
+const fs = require('fs');
+const path = require('os').homedir() + '/new/artifact/file.txt'; // 生成文件路径
+
 const paths = process.env.ARTIFACT_PATHS;
 const parsedPaths = JSON.parse(paths);
 const artifactPaths = Array.isArray(parsedPaths) ? parsedPaths : [parsedPaths]; // 将单个路径转为数组
 // console.log(artifactPaths.join("\n"));
 const result = artifactPaths.join("\n");
 
-process.env.NEW_ARTIFACT_PATHS = result;
-console.log("NEW_ARTIFACT_PATHS: ",process.env.NEW_ARTIFACT_PATHS);
+// 创建目录以及写入文件
+fs.mkdirSync(path.split('/').slice(0, -1).join('/'), { recursive: true }); // 递归创建目录
+fs.writeFileSync(path, result); // 将 result 写入文件
 
+console.log("NEW_ARTIFACT_PATHS: ",process.env.NEW_ARTIFACT_PATHS);
 console.log(result); // 输出到控制台
 export default result;
